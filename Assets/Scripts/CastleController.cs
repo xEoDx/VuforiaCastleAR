730841bb5437666f8c105b58f1be92f310d1e76a
Victor <victor.alcazar.alcazar@gmail.com>
2017-08-14 21:15:21 +0200

    Update CastleController

--- Assets/Scripts/CastleController.cs
+++ Assets/Scripts/CastleController.cs
@@ -5,22 +5,19 @@ using Vuforia;
 
 public class CastleController : MonoBehaviour {
 
+
+
+    #region PRIVATE_MEMBERS
+
     [SerializeField]
     private UserDefinedTargetHandler udtEventHandler;
-    
-    [SerializeField]
-    private GameObject msgFindThePenguin;
-    [SerializeField]
-    private GameObject msgTapThePenguin;
-    [SerializeField]
-    private GameObject msgTapTheCircle;
-    
+   
+    /*[SerializeField]
+    private GameObject msgGetCloser;*/
     [SerializeField]
     private GameObject msgModeDT;
     [SerializeField]
     private GameObject msgModeUDT;
-
-    private Camera cam;
 
     private enum TrackingMode
     {
@@ -31,31 +28,190 @@ public class CastleController : MonoBehaviour {
 
     // initial mode
     private TrackingMode mTrackingMode = TrackingMode.DEVICE_ORIENTATION;
-    private GameObject castleObject;
-    private float mInitialDistance = 2.5f;
 
-    // Use this for initialization
-    void Start () {
+    private Vector3 mPosOffsetAtTargetCreation;
+
+    private const float mInitialDistance = 2.5f;
+
+    private bool mBuildingUDT = false;
+
+    private Camera cam;
+
+    private GameObject penguinModel;
+    private GameObject penguinShadow;
+    private bool startCheckingFrameQualityForNewUDT;
+
+    #endregion //PRIVATE_MEMBERS
+
+    public void StartUDTTracker()
+    {
+        if (!startCheckingFrameQualityForNewUDT)
+            startCheckingFrameQualityForNewUDT = true;
+    }
+
+    #region MONOBEHAVIOUR_METHODS
+
+    void Awake()
+    {
+        //msgGetCloser.SetActive(false);
+
+        msgModeDT.SetActive(true);
+        msgModeUDT.SetActive(false);
+        startCheckingFrameQualityForNewUDT = false;
+    }
+
+    void Start()
+    {
         mTrackingMode = TrackingMode.DEVICE_ORIENTATION;
 
         VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+        udtEventHandler.ShowQualityIndicator(false); // don't show at start
+
+        penguinModel = GameObject.Find("PenguinModel");
+        penguinShadow = GameObject.Find("Penguin_Shadow");
+    }
+
+    void Update()
+    {
+        if (startCheckingFrameQualityForNewUDT)
+        {
+            ChangeMode();
+        }
+    }
+    /*
+    void Update()
+    {
+        if (CheckTapOnObject())
+        {
+            ChangeMode();
+        }
+
+        if ((Screen.orientation == ScreenOrientation.Portrait) && (Screen.width < Screen.height))
+        {
+            msgModeDT.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 150.0f);
+            msgModeUDT.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 150.0f);
+        }
+        else
+        {
+            msgModeDT.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 30.0f);
+            msgModeUDT.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 30.0f);
+        }
+    }*/
+
+    void LateUpdate()
+    {
+
+        switch (mTrackingMode)
+        {
+            case TrackingMode.CONSTRAINED_TO_CAMERA:
+                {
+                    // In this phase, the Penguin is constrained to remain
+                    // in the camera view, so it follows the user motion
+                    Vector3 constrainedPos = cam.transform.position + cam.transform.forward * mInitialDistance;
+                    this.transform.position = constrainedPos;
+
+                    // Update object rotation so that it always look towards the camera
+                    // and its "up vector" is always aligned with the gravity direction.
+                    // NOTE: since we are using DeviceTracker, the World up vector is guaranteed 
+                    // to be aligned (approximately) with the real world gravity direction 
+                    //RotateToLookAtCamera();
+
+                    // Check if we were waiting for a UDT creation,
+                    // and switch mode if UDT was created
+                    if (mBuildingUDT && udtEventHandler && udtEventHandler.TargetCreated)
+                    {
+
+                        ImageTargetBehaviour trackedTarget = GetActiveTarget();
+
+                        if (trackedTarget != null)
+                        {
+                            mBuildingUDT = false;
+
+                            // Switch mode to UDT based tracking
+                            mTrackingMode = TrackingMode.UDT_BASED;
+
+                            // Update header text
+                            //DisplayMessage(msgGetCloser);
+                            DisplayModeLabel(msgModeUDT);
+
+                            // Hide quality indicator
+                            udtEventHandler.ShowQualityIndicator(false);
+
+                            // Show the penguin
+                            ShowPenguin(true);                            
+
+                            // Save a snapshot of the current position offset
+                            // between the object and the target center
+                            mPosOffsetAtTargetCreation = this.transform.position - trackedTarget.transform.position;
+                        }
+                    }
+                }
+                break;
+            case TrackingMode.UDT_BASED:
+                {
+                    // Update the object world position according to the UDT target position
+                    ImageTargetBehaviour trackedTarget = GetActiveTarget();
+                    if (trackedTarget != null)
+                    {
+                        this.transform.position = trackedTarget.transform.position + mPosOffsetAtTargetCreation;
+                    }
+
+                    // Update object rotation so that it always look towards the camera
+                    // and its "up vector" is always aligned with the gravity direction.
+                    // NOTE: since we are using DeviceTracker, the World up vector is guaranteed 
+                    // to be aligned (approximately) with the real world gravity direction 
+                    //RotateToLookAtCamera();
+                }
+                break;
+        }
+    }
+
+    #endregion //MONOBEHAVIOUR_METHODS
+
+
+
+    #region PUBLIC_METHODS
+
+    public void ResetState()
+    {
+        mTrackingMode = TrackingMode.DEVICE_ORIENTATION;
+        mBuildingUDT = false;
+
+        
+        DisplayModeLabel(msgModeDT);
+
+        // Hide the quality indicator
+        udtEventHandler.ShowQualityIndicator(false);
+
+        // Show the penguin
+        ShowPenguin(true);
+
+       
+    }
+
+    #endregion //PUBLIC_METHODS
+
+
+    #region PRIVATE_METHODS
 
     // Callback called when Vuforia has started
     private void OnVuforiaStarted()
     {
         cam = Vuforia.DigitalEyewearARController.Instance.PrimaryCamera ?? Camera.main;
+
+        StartCoroutine(ResetAfter(0.5f));
     }
 
+    private IEnumerator ResetAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        ResetState();
+    }
 
     private void ChangeMode()
     {
+        
         if (mTrackingMode == TrackingMode.DEVICE_ORIENTATION)
         {
             SwitchToCameraMode();
@@ -70,16 +226,15 @@ public class CastleController : MonoBehaviour {
     {
         if (mTrackingMode == TrackingMode.DEVICE_ORIENTATION)
         {
-
             mTrackingMode = TrackingMode.CONSTRAINED_TO_CAMERA;
 
-           
+            DisplayModeLabel(msgModeUDT);
 
             // Show the quality indicator
             udtEventHandler.ShowQualityIndicator(true);
 
             // Hide the penguin
-            ShowCastle(false);
+            ShowPenguin(false);
         }
     }
 
@@ -97,8 +252,7 @@ public class CastleController : MonoBehaviour {
             }
             else
             {
-                //DisplayMessage(msgTryAgain);
-                //DisplayModeLabel(msgModeUDT);
+                DisplayModeLabel(msgModeUDT);
             }
         }
     }
@@ -115,8 +269,30 @@ public class CastleController : MonoBehaviour {
             targetWidth = 2.0f * halfSizeX;
         }
 
-        //mBuildingUDT = true;
+        mBuildingUDT = true;
         udtEventHandler.BuildNewTarget(targetWidth);
+        startCheckingFrameQualityForNewUDT = false;
+    }
+
+    /*private void RotateToLookAtCamera()
+    {
+        Vector3 objPos = this.transform.position;
+        Vector3 objGroundPos = new Vector3(objPos.x, 0, objPos.z); // y = 0
+        Vector3 camGroundPos = new Vector3(cam.transform.position.x, 0, cam.transform.position.z);
+        Vector3 objectToCam = camGroundPos - objGroundPos;
+        objectToCam.Normalize();
+        this.transform.rotation *= Quaternion.FromToRotation(this.transform.forward, objectToCam);
+    }*/
+
+    /*private void DisplayMessage(GameObject messageObj)
+    {
+        //msgGetCloser.SetActive((msgGetCloser == messageObj));
+    }*/
+
+    private void DisplayModeLabel(GameObject modeObj)
+    {
+        msgModeDT.SetActive((msgModeDT == modeObj));
+        msgModeUDT.SetActive((msgModeUDT == modeObj));
     }
 
     private ImageTargetBehaviour GetActiveTarget()
@@ -133,11 +309,14 @@ public class CastleController : MonoBehaviour {
         return null;
     }
 
-    private void ShowCastle(bool isVisible)
+    private void ShowPenguin(bool isVisible)
     {
-        if (castleObject != null )
+        if (penguinModel != null && penguinShadow != null)
         {
-            castleObject.GetComponent<Renderer>().enabled = isVisible;
+            penguinModel.GetComponent<Renderer>().enabled = isVisible;
+            penguinShadow.GetComponent<Renderer>().enabled = isVisible;
         }
     }
+
+    #endregion //PRIVATE_METHODS
 }
